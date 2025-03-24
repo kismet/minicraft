c1d20d6555d7fa310e6af6b353d7367a77e99375
Christian Conti <christian.conti.2007@gmail.com>
2025-03-24 18:44:31 +0100

    added counter blocks UI

--- Assets/Scripts/HotBarManager.cs
+++ Assets/Scripts/HotBarManager.cs
@@ -1,13 +1,29 @@
 using UnityEngine;
+using TMPro;
+using System.Reflection;
+using System.Collections.Generic;
 
 public class HotBarManager : MonoBehaviour
 {
     public GameObject[] hotbarItems = new GameObject[9];
+    public TMP_Text[] quantityTexts;
     public int currentSlotIndex = 0;
+
+    private BlockInteraction blockInteraction;
+    private Dictionary<string, int> inventory;
+
+    void Start()
+    {
+        blockInteraction = FindObjectOfType<BlockInteraction>();
+        FieldInfo fieldInfo = typeof(BlockInteraction).GetField("inventory",
+            BindingFlags.NonPublic | BindingFlags.Instance);
+        inventory = (Dictionary<string, int>)fieldInfo.GetValue(blockInteraction);
+    }
 
     void Update()
     {
         HandleHotbarSelection();
+        UpdateHotbarUI();
     }
 
     void HandleHotbarSelection()
@@ -20,6 +36,19 @@ public class HotBarManager : MonoBehaviour
         {
             if (Input.GetKeyDown((i + 1).ToString()))
                 currentSlotIndex = i;
+        }
+    }
+
+    void UpdateHotbarUI()
+    {
+        for (int i = 0; i < hotbarItems.Length; i++)
+        {
+            if (hotbarItems[i] != null && i < quantityTexts.Length && quantityTexts[i] != null)
+            {
+                string blockType = hotbarItems[i].tag;
+                int count = inventory != null && inventory.TryGetValue(blockType, out count) ? count : 0;
+                quantityTexts[i].text = count.ToString();
+            }
         }
     }
 }
