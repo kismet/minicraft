c72e4d3db94caa36b9da19b18cc7359205aaddf6
IIFlynz-png <cavallinipavel097@gmail.com>
2025-04-08 11:21:39 +0200

    Added diamondID
    This scripts is use to assing an ID to the diamonds

--- Assets/Scripts/DiamondID.cs
+++ Assets/Scripts/DiamondID.cs
@@ -2,17 +2,18 @@ using UnityEngine;
 
 public class DiamondID : MonoBehaviour
 {
-    // Definisci un ID univoco per questo tipo di diamante.
+    // Definisci un ID numerico univoco per questo tipo di diamante.
     // Impostalo nell'Inspector per ogni prefab/istanza di diamante.
-    // Esempi: "ForestDiamond", "DesertDiamond", "IceDiamond", "VolcanoDiamond"
-    public string uniqueID;
+    // Esempio: 1 per Foresta, 2 per Deserto, 3 per Ghiaccio, 4 per Vulcano.
+    // Assicurati che 0 non sia usato o sia considerato non valido se necessario.
+    public int uniqueID;
 
     void Start()
     {
-        // Validazione: Assicurati che un ID sia stato assegnato nell'Inspector
-        if (string.IsNullOrEmpty(uniqueID))
+        // Validazione: Assicurati che un ID valido (es. > 0) sia stato assegnato
+        if (uniqueID <= 0) // O qualsiasi altra logica di validazione (es. uniqueID == 0)
         {
-            Debug.LogError($"Il diamante '{gameObject.name}' non ha un uniqueID assegnato!", gameObject);
+            Debug.LogError($"Il diamante '{gameObject.name}' non ha un uniqueID valido assegnato (deve essere > 0)!", gameObject);
         }
     }
 }
